642685b455b789110bccf49a57c17a1c0224cf3d
qminhminh <hqminh050503@gmail.com>
2024-02-09 17:44:23 +0700

    gbvfbd

---
+++ firebase_config.js
@@ -0,0 +1,10 @@
+// firebase_config.js
+export const firebaseConfig = {
+    apiKey: "AIzaSyB_tdM9rr1lgoVmrWxprGmos1GiRA4dNoc",
+    authDomain: "tiktokclone-7ad13.firebaseapp.com",
+    databaseURL: "https://tiktokclone-7ad13-default-rtdb.firebaseio.com",
+    projectId: "tiktokclone-7ad13",
+    storageBucket: "tiktokclone-7ad13.appspot.com",
+    messagingSenderId: "281132359702",
+    appId: "1:281132359702:web:1e6d32ee5cad955c2cd537"
+  };
